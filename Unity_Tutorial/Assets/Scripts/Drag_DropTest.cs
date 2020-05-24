c00b0aeab6db2cd2d3651876ef55bbc0429a9154
HMu0510 <HMu0510@gmail.com>
2020-05-24 23:07:04 +0900

    23.DragDrop

---
+++ Unity_Tutorial/Assets/Scripts/Drag_DropTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag_DropTest : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IDropHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Begin Drag");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        Debug.Log("Drag");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop");
+        //throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("End Drag");
+        //throw new System.NotImplementedException();
+    }
+}
