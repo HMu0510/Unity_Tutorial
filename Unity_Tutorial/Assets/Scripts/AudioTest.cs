b54a522f44dbfb3dc21abf6551d2d37e27bab34b
HMu0510 <HMu0510@gmail.com>
2020-05-24 18:31:54 +0900

    16.Audio

---
+++ Unity_Tutorial/Assets/Scripts/AudioTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTest : MonoBehaviour
+{
+    private AudioSource theAudio;
+
+    [SerializeField] private AudioClip[] clip;
+
+    private void Start()
+    {
+        theAudio = GetComponent<AudioSource>();
+    }
+
+    public void PlaySE()
+    {
+        int _temp = Random.Range(0, 4);
+
+        theAudio.clip = clip[_temp];
+        theAudio.Play();
+    }
+}
